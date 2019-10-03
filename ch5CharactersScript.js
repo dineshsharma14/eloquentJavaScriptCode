d521494be09401be77990d27a55c281925e2f026
dsharma <dinesh.sw3@gmail.com>
2019-10-03 14:10:02 -0500

    Added program to find the percentage of particular script used in a given random piece of text

--- ch5CharactersScript.js
+++ ch5CharactersScript.js
@@ -1115,65 +1115,8 @@ var SCRIPTS = [
     }
   ];
 
-// Reduce is one of the key operations we do JS arrays apart from Filter and Map
-// This is a common example of finding some of all the nos in an array.
 
-function reduce(array, combine, start) {
-    let counter = start;
-    for (let element of array){
-    counter = combine(element, counter);
-    }
-    return counter;
-}
-
-console.log(reduce([1,2,3,4], (a,b) => a + b, 0));
-console.log([1,2,3,4].reduce((a,b) => {return a + b}, 0));
-
-// Finding the script with most number of chars
-
-function characterCount(script){
-    return script.ranges.reduce((count, [from, to]) => {
-        return count + (to - from);
-    }, 0);
-}
-
-// Doing kinda bubble sort here!
-console.log(SCRIPTS.reduce((a, b) => {
-    return characterCount(a) < characterCount(b) ? b : a;
-}));
-
-// Syntax of reduce method on arrays
-// array.reduce(function( total, currentValue, currentIndex, arr), initialValue)
-// it is mandatory to pass fx as argument when using reduce method 
-// in the inner fx its mandatory to define total and currentValue!
-
-// few more examples of application of reduce method!
-
-let someNumbers = [15.5, 2.3, 1.1, 4.7];
-console.log("Sum of nos. in someNumbers array: " + 
-  someNumbers.reduce((total, num) => total + num,0));
-
-function average(array) {
-    return array.reduce((a, b) => a + b) / array.length;
-}
-
-console.log("The average (using reduce method on array) of nos 1-5 is: " + 
-  average([1,2,3,4,5]));
-
-let intArray = [1,2,3,4,5];
-let sum = 0;
-for (element of intArray){
-    sum += element;
-}
-console.log("The average (using standard programmatic way) of nos 1-5 is: " + 
-  sum / intArray.length);
-
-console.log(Math.round(average(
-    SCRIPTS.filter(s => s.living).map(s => s.year)
-)));
-console.log(Math.round(average(
-    SCRIPTS.filter(s => !s.living).map(s => s.year)
-)));
+// Write a program which can find the Script the random text passed belong to.
 
 // Code for finding the script when a random text is provided!
 // Let's break the problem and think if we are passed the Unicode under usage!
@@ -1190,3 +1133,46 @@ function characterScript(code) {
 }
 
 console.log(characterScript(34768));
+
+// Use for/of loop to iterate over characters using 2 code units!
+
+// Now let's write a fx that counts characters belong to particular script.
+function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1){
+            counts.push({name, count: 1});
+        }
+        else {
+            counts[known].count++;
+        }
+    }
+    return counts;
+}
+
+// testing countBy
+console.log(countBy([1,2,3,4,5], n => n > 2 ));
+
+// Finally after doing basic abstractions and breaking the problem in parts
+// functionally let's write the fx which actually parse on text passed!
+
+function textScripts(text) {
+    
+    let scripts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.name : "none";
+    }).filter(({name}) => name != "none");
+
+    let total = scripts.reduce((n, {count}) => n + count, 0);
+    if (total == 0) return "No scripts found.";
+
+    return scripts.map(({name, count}) => {
+        return `${Math.round(count * 100 / total)}% ${name}`;
+    }).join(", ");
+    
+}
+
+console.log(textScripts('英国的狗说"woof woof", 俄罗斯的狗说"тяв"'+
+'देश में बिजली की समस्या भी होगी दूर'));
